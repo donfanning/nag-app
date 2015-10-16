3bde4365458de0893d038fcc8c3884881ff3c65d
Alexei Zverev <alzverev@cisco.com>
2015-10-16 16:49:49 -0700

    nodesets id fixes

--- backend/topology-data.js
+++ backend/topology-data.js
@@ -1,9 +1,9 @@
 module.exports = {
 	response: {
 		nodes: [
-			{id: 0, name: "Root Router AS-1", x: 100, y: 100,group:100},
-			{id: 1, name: "Root Router AS-2", x: 70, y: 150,group:101},
-			{id: 2, name: "Root Router AS-3", x: 130, y: 150,group:102}
+			{id: 0, name: "Root Router AS-1", x: 100, y: 100, group:0},
+			{id: 1, name: "Root Router AS-2", x: 70, y: 150, group:1},
+			{id: 2, name: "Root Router AS-3", x: 130, y: 150, group:2}
 		],
 		links: [
 			{id: 0, source: 0, target: 1},
