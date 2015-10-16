b74644f5eca6cf839100a5a5e2b8960da8e90dd9
Alexei Zverev <alzverev@cisco.com>
2015-10-15 23:13:48 -0700

    improved scalability

--- js/next-modules/data.js
+++ js/next-modules/data.js
@@ -29,7 +29,11 @@ var odl2next = function (data) {
 			// add the node to the result object
 			topologyResult.nodes.push(node);
 		}
+/*
 
+ x: Math.floor(Math.random() * 800 + 10),
+ y: Math.floor(Math.random() * 400 + 10),
+ */
 		// processing links
 		for (i = 0; i < topology.link.length; i++) {
 			var link = {
@@ -45,4 +49,46 @@ var odl2next = function (data) {
 		alert('JSON response with topology data is not valid.\nVerify you REST API and server-side application.');
 	}
 	return topologyResult;
+};
+
+// implementing an async http request
+var loadJSON = function(app,topology) {
+	$.ajax({
+		url: "http://localhost:5555",
+		type: 'GET',
+		contentType: 'application/json',
+		// as soon as scripts receives valid result, this function will be run
+		success: function (data) {
+			// process ODL topology's JSON to turn it to next json
+			topologyData = odl2next(data);
+			// feed topology object with nodes and links...
+			topology.data(topologyData);
+			// ... then attach the topology to the app instance
+			topology.attach(app);
+		},
+		// errors will never pass silently
+		error: function (jqXHR, exception) {
+			if (jqXHR.status === 0) {
+				//alert('Not connect.\nVerify Network.');
+			}
+			else if (jqXHR.status == 404) {
+				alert('Requested page not found. [404]');
+			}
+			else if (jqXHR.status == 500) {
+				alert('Internal Server Error [500].');
+			}
+			else if (exception === 'parsererror') {
+				alert('Requested JSON parse failed.');
+			}
+			else if (exception === 'timeout') {
+				alert('Time out error.');
+			}
+			else if (exception === 'abort') {
+				alert('Ajax request aborted.');
+			}
+			else {
+				alert('Uncaught Error.\n' + jqXHR.responseText);
+			}
+		}
+	});
 };
--- js/shell.js
+++ js/shell.js
@@ -10,47 +10,7 @@ Main module that manages the topology app
 	var topology = topologyContainer.topology();
 	//assign the app to the <div>
 	app.container(document.getElementById('next-app'));
-
-	// implementing an async http request
-	$.ajax({
-		url: "http://localhost:5555",
-		type: 'GET',
-		contentType: 'application/json',
-
-		// as soon as scripts receives valid result, this function will be run
-		success: function (data) {
-			// process ODL topology's JSON to turn it to next json
-			topologyData = odl2next(data);
-			// feed topology object with nodes and links...
-			topology.data(topologyData);
-			// ... then attach the topology to the app instance
-			topology.attach(app);
-		},
-
-		// errors will never pass silently
-		error: function(jqXHR, exception){
-			if (jqXHR.status === 0) {
-				alert('Not connect.\nVerify Network.');
-			}
-			else if (jqXHR.status == 404) {
-				alert('Requested page not found. [404]');
-			}
-			else if (jqXHR.status == 500) {
-				alert('Internal Server Error [500].');
-			}
-			else if (exception === 'parsererror') {
-				alert('Requested JSON parse failed.');
-			}
-			else if (exception === 'timeout') {
-				alert('Time out error.');
-			}
-			else if (exception === 'abort') {
-				alert('Ajax request aborted.');
-			}
-			else {
-				alert('Uncaught Error.\n' + jqXHR.responseText);
-			}
-		}
-	});
+	loadJSON(app,topology);
+	setInterval(loadJSON,1000,app,topology);
 
 })(nx);
